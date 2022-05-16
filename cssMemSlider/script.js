f690799266cc766d48dbf92479401a89b0c88a62
janChorny <janchorny@tut.by>
2022-05-16 17:16:29 +0300

    feat: add flexability to slider (Mon, May 16, 2022 05:16:29 PM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -3,20 +3,40 @@
 const screen = document.querySelector('.main__screen');
 const slider = document.querySelector('.slider');
 const text = document.querySelector('.main__text');
-const controls = document.querySelectorAll('.controls-main__input');
+const controls = document.querySelectorAll('.main__controls-item');
+const controlsInner = document.querySelectorAll('.main__controls-inner'); 
 
- function moveLeft() {
+function moveLeft() {
  	slider.classList.add('transition-left');
 	 controls.forEach(element => {
 		 element.removeEventListener('click', moveLeft);
 	 });
  }
 
+controls.forEach((el) => el.addEventListener('mouseover', ()=>{
+	// el.firstChild.classList.add('hover');
+	console.log(el.firstChild);
+}));
+
+controls.forEach((el) => el.addEventListener('mouseout', () => {
+	// el.firstChild.classList.remove('hover');
+}));
+
+
 controls[0].addEventListener('click', () => {
+	controlsInner[0].classList.add('active');
+	controlsInner.forEach(element => {
+		if (element !== controlsInner[0]){
+			element.classList.remove('active');
+		}
+	});
 	let slideRight = document.createElement('div');
 	slideRight.classList.add('slider__item');
 	slider.append(slideRight);
-	slideRight.innerHTML = '<img src="./assets/images/fixing_the_bug.jpg" width="700" height ="700">';
+	let image = document.createElement('div');
+	image.classList.add('image');
+	image.classList.add('image--one');
+	slideRight.append(image);
 	slideRight.value = "yes...yesterday found a new bug, fixing it";
 	text.textContent = slideRight.value;
 	text.classList.add('transition-text-left');
@@ -35,10 +55,19 @@ controls[0].addEventListener('click', () => {
 });
 
 controls[1].addEventListener('click',()=>{
+	controlsInner[1].classList.add('active');
+	controlsInner.forEach(element => {
+		if (element !== controlsInner[1]) {
+			element.classList.remove('active');
+		}
+	});
 	let slideRight = document.createElement('div');
 	slideRight.classList.add('slider__item');
-	slider.append(slideRight);	
-	slideRight.innerHTML = '<img src="./assets/images/music.jpg" width="700" height ="700">';
+	slider.append(slideRight);
+	let image = document.createElement('div');
+	image.classList.add('image');
+	image.classList.add('image--two');
+	slideRight.append(image);
 	slideRight.value = "Каждый программист немного музыкант";
 	text.textContent = slideRight.value;
 	text.classList.add('transition-text-left');
@@ -56,10 +85,19 @@ controls[1].addEventListener('click',()=>{
 });
 
 controls[2].addEventListener('click', () => {
+	controlsInner[2].classList.add('active');
+	controlsInner.forEach(element => {
+		if (element !== controlsInner[2]) {
+			element.classList.remove('active');
+		}
+	});
 	let slideRight = document.createElement('div');
 	slideRight.classList.add('slider__item');
 	slider.append(slideRight);
-	slideRight.innerHTML = '<img src="./assets/images/programmer.jpg" width="700" height ="700">';
+	let image = document.createElement('div');
+	image.classList.add('image');
+	image.classList.add('image--three');
+	slideRight.append(image);
 	slideRight.value = "Ты ж программист!...";
 	text.textContent = slideRight.value;
 	text.classList.add('transition-text-left');
@@ -78,10 +116,19 @@ controls[2].addEventListener('click', () => {
 });
 
 controls[3].addEventListener('click', () => {
+	controlsInner[3].classList.add('active');
+	controlsInner.forEach(element => {
+		if (element !== controlsInner[3]) {
+			element.classList.remove('active');
+		}
+	});
 	let slideRight = document.createElement('div');
 	slideRight.classList.add('slider__item');
 	slider.append(slideRight);
-	slideRight.innerHTML = '<img src="./assets/images/usb.jpg" width="700" height ="700">';
+	let image = document.createElement('div');
+	image.classList.add('image');
+	image.classList.add('image--four');
+	slideRight.append(image);
 	slideRight.value = "How to insert usb";
 	text.textContent = slideRight.value;
 	moveLeft();
